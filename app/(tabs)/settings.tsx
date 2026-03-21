7151fd75bdc792528e877bb3ae55a8fa6cfc9a11
Sentharion <michpawbert@gmail.com>
2026-03-21 17:04:30 +0100

    First settings changes, units, location, notifications and privacy policy added

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,10 @@
-import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import SettingsScreen from "../screens/SettingsScreen";
 
 export default function Settings() {
     return (
         <SafeAreaView>
-            <Text>Settings</Text>
+            <SettingsScreen />
         </SafeAreaView>
     );
 }
--- app/screens/SettingsScreen.tsx
+++ app/screens/SettingsScreen.tsx
@@ -1,9 +1,27 @@
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import SettingsHeader from "../components/settings/SettingsHeader";
+import Units from "../components/settings/Units";
+import LocationAndNotifications from "../components/settings/LocationAndNotifications";
+import PrivacyPolicy from "../components/settings/PrivacyPolicy";
 
 const SettingsScreen = () => {
     return (
         <ScrollView className="bg-gray-50">
-            <Text>SettingsScreen</Text>
+            <View className="py-10 px-1">
+                <LinearGradient
+                    colors={['#3b82f6', '#06b6d4']}
+                    className='absolute inset-0'
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                />
+                <SettingsHeader />
+            </View>
+            <View className="px-7 -mt-7 flex-col gap-5">
+                <Units />
+                <LocationAndNotifications />
+                <PrivacyPolicy />
+            </View>
         </ScrollView>
     );
 };
