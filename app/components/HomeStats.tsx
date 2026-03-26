5f902db4308480ce70c9f8e4e8f5590b1f7d8a05
Sentharion <michpawbert@gmail.com>
2026-03-26 15:44:24 +0100

    homestatsUpdate

--- app/components/HomeStats.tsx
+++ app/components/HomeStats.tsx
@@ -1,27 +1,30 @@
 import { View, Text } from 'react-native';
+import { useSavedWalkStore } from '@/store/savedStore';
+import { getDailyStats } from '@/utils/stats';
 
-const distance = 3.2;
-const time = 30;
-const steps = 10000;
-const calories = 215;
 
 const HomeStats = () => {
+    const walks = useSavedWalkStore(state => state.savedWalks);
+    const stats = getDailyStats(walks);
+    const todayIndex = (new Date().getDay() + 6) % 7;
+    const todayStats = stats[todayIndex];
+
     return (
         <View className='items-start justify-center bg-white/15 overflow-hidden px-3 py-4 rounded-xl m-6'>
             <Text className='text-sm text-white mb-2 pl-2'>Dzisiejsza aktywność:</Text>
-            <Text className='text-lg text-white mb-4 pl-2'><Text className='font-semibold text-3xl'>{distance}</Text> km pokonane</Text>
+            <Text className='text-lg text-white mb-4 pl-2'><Text className='font-semibold text-3xl'>{todayStats.distance}</Text> km pokonane</Text>
             <View className='flex-row gap-3 mx-2 flex-wrap'>
                 <View className='flex-1 items-start justify-center bg-white/20 overflow-hidden p-3 rounded-lg gap-1'>
                     <Text className='text-xs text-white'>Czas</Text>
-                    <Text className='text-md font-bold text-white'><Text className='font-bold text-lg'>{time}</Text> min</Text>
+                    <Text className='text-md font-bold text-white'><Text className='font-bold text-lg'>{todayStats.duration}</Text> min</Text>
                 </View>
                 <View className='flex-1 items-start justify-center bg-white/20 overflow-hidden p-3 rounded-lg gap-1'>
                     <Text className='text-xs text-white'>Kroki</Text>
-                    <Text className='text-md font-bold text-white'><Text className='font-bold text-lg'>{steps}</Text></Text>
+                    <Text className='text-md font-bold text-white'><Text className='font-bold text-lg'>{todayStats.steps}</Text></Text>
                 </View>
                 <View className='flex-1 items-start justify-center bg-white/20 overflow-hidden p-3 rounded-lg gap-1'>
                     <Text className='text-xs text-left text-white'>Spalone kalorie</Text>
-                    <Text className='text-md font-bold text-white'>{calories} kcal</Text>
+                    <Text className='text-md font-bold text-white'>{todayStats.calories} kcal</Text>
                 </View>
             </View>
         </View>
